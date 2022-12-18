73dd695533b47d372a9cf2c4faf57f6b7f46efcf
Stanislav <OrlovskiyStanislaw@gmail.com>
2022-12-18 11:54:21 +0300

    - create new app "users" 1) Create CustomUser model 2) Create CourierUser model 3) Add in settings AUTH_USER_MODEL = 'users.CustomUser' 4) Add in settings.py static and media root

---
+++ courierservice/users/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(
+        'Никнейм',
+        db_index=True,
+        max_length=255,
+        unique=True
+    )
+    email = models.EmailField(
+        'Почта',
+        max_length=254,
+        db_index=True,
+        unique=True
+    )
+    first_name = models.CharField('Имя', max_length=150)
+    last_name = models.CharField('Фамилия', max_length=150)
+    bio = models.TextField('Описание', max_length=500, blank=True)
+    is_courier = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class CourierUser(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        verbose_name='Пользователь'
+    )
+    orders = models.ManyToManyField(
+        Order,
+        related_name='orders_of_current_courier',
+        verbose_name='Заказы'
+    )
+    image = models.ImageField(
+        upload_to='photo_of_couriers/',
+        verbose_name='Изображения'
+    )
+
