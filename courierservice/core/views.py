6384fe6337a5a1eb243943c890438ad0d9913cf1
Stanislav <OrlovskiyStanislaw@gmail.com>
2022-12-21 20:10:00 +0300

    FIX REGISTRATION - now password set to current user

--- courierservice/core/views.py
+++ courierservice/core/views.py
@@ -1,3 +1,16 @@
+# core/views.py
 from django.shortcuts import render
 
-# Create your views here.
+
+def page_not_found(request, exception):
+    # Переменная exception содержит отладочную информацию;
+    # выводить её в шаблон пользовательской страницы 404 мы не станем
+    return render(request, '', {'path': request.path}, status=404)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, '')
+
+
+def handler500(request):
+    return render(request, '', status=500)
